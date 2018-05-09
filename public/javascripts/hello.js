cd0f6cc440f800cb796ec0f72efd7cfd6833fdf4
WangSen <wsgoodman@163.com>
2018-05-09 18:05:20 +0800

    获取数据页面与数据详情页面。

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,21 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+
+var indexs = document.getElementsByClassName('index');
+var dates = document.getElementsByClassName('dateValue');
+var buttons = document.getElementsByClassName('checkDetail');
+for (var i = 0,len = buttons.length;i< len;i++) {
+  var indexValue = indexs[i];
+  var dateValue = dates[i];
+    var daydate = new Date(dateValue.innerHTML - 0);
+    dateValue.innerHTML = daydate.getFullYear() +"-"+ (daydate.getMonth() + 1 )+"-" + daydate.getDate();
+    buttons[i].onclick = (function(k){
+        var id = indexValue.innerHTML;
+        return function(){
+            window.location.href = "/admin/query/provider/apply/"+id;
+        };
+    })(i);
+
+}
+
+window.onload = function() {
+
 }
