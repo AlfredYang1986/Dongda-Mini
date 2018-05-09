a1db5f303044eb01382aa8e4cd509d514bab75a8
AlfredYang1986 <yangyuanpig@gmail.com>
2018-05-09 15:45:48 +0800

    用户出版 等数据结构

---
+++ app/bmlogic/user/UserData/creation.scala
@@ -0,0 +1,40 @@
+package bmlogic.user.UserData
+
+import java.util.Date
+
+import com.mongodb.casbah.Imports._
+import org.bson.types.ObjectId
+import play.api.libs.json.JsValue
+
+trait creation {
+    implicit val m2d : JsValue => DBObject = { js =>
+        val data = (js \ "apply").asOpt[JsValue].get
+        val wechat_user = (data \ "wechat_user").asOpt[JsValue].get
+
+        val wechat_builder = MongoDBObject.newBuilder
+        wechat_builder += "wechat_name" -> (wechat_user \ "wechat_name").asOpt[String].get
+        wechat_builder += "wechat_photo" -> (wechat_user \ "wechat_photo").asOpt[String].get
+        wechat_builder += "wechat_open_id" -> (wechat_user \ "wechat_open_id").asOpt[String].get
+        wechat_builder += "wechat_token" -> (wechat_user \ "wechat_token").asOpt[String].get
+
+//        val age_boundary = MongoDBObject.newBuilder
+//        age_boundary += "low" -> (data \ "age_boundary" \ "low").asOpt[Int].get
+//        age_boundary += "up" -> (data \ "age_boundary" \ "up").asOpt[Int].get
+
+        val builder = MongoDBObject.newBuilder
+        builder += "_id" -> ObjectId.get()
+//        builder += "age_boundary" -> age_boundary.result
+        builder += "wechat" -> wechat_builder.result
+        builder += "address" -> (data \ "address").asOpt[String].map(x => x).getOrElse("")
+//        builder += "contact" -> (data \ "contact").asOpt[String].map(x => x).getOrElse("")
+//        builder += "contact_no" -> (data \ "contact_no").asOpt[String].map(x => x).getOrElse("")
+//        builder += "brand_name" -> (data \ "brand_name").asOpt[String].map(x => x).getOrElse("")
+//        builder += "service_type" -> (data \ "service_type").asOpt[String].map(x => x).getOrElse("")
+//        builder += "service_leaf" -> (data \ "service_leaf").asOpt[String].map(x => x).getOrElse("")
+//        builder += "input_pwd" -> (data \ "input_pwd").asOpt[String].map (x => x).getOrElse("")
+        builder += "approved" -> 0
+        builder += "date" -> new Date().getTime
+
+        builder.result
+    }
+}
