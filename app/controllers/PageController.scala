ccb220bd924ddcd57a3558ab39fa7e72949a0478
AlfredYang1986 <yangyuanpig@gmail.com>
2018-05-20 18:16:26 +0800

    运营逻辑页面

--- app/controllers/PageController.scala
+++ app/controllers/PageController.scala
@@ -1,27 +1,16 @@
 package controllers
 
-import akka.actor.ActorSystem
-import bmlogic.miniauth.MiniAuthMessage.{msg_dropWXUnwanted, msg_queryWXAuthPara}
-import bmlogic.user.UserMessage._
-import com.pharbers.bmmessages.{CommonModules, MessageRoutes}
-import com.pharbers.bmpattern.LogMessage.msg_log
-import com.pharbers.bmpattern.ResultMessage.msg_CommonResultMessage
-import com.pharbers.dbManagerTrait.dbInstanceManager
-import com.pharbers.token.AuthTokenTrait
-import controllers.common.requestArgsQuery
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.Action
-import play.api.libs.json.Json.toJson
+import play.api.mvc.{Action, Controller}
 
 @Singleton
-class UserController @Inject() (as_inject: ActorSystem, dbt : dbInstanceManager, att : AuthTokenTrait) {
-    implicit val as: ActorSystem = as_inject
-    lazy val raq = requestArgsQuery()
+class PageController @Inject() extends Controller {
 
-    import com.pharbers.bmpattern.LogMessage.common_log
-    import com.pharbers.bmpattern.ResultMessage.common_result
-
-	def business = Action {
+    def business = Action {
 		Ok(views.html.business())
 	}
+
+    def activity(provider_id : String) = Action {
+        Ok(views.html.activity(provider_id))
+    }
 }
